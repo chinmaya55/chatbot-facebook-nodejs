d620b8c6dd4955b4dc105268e698bc0c2e178398
Chinmaya Kumar <chinmayakumar55@gmail.com>
2017-12-12 15:45:18 +0530

    integrated developer.citi

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.get('/', function (req, res) {
 		json: 'grant_type=authorization_code&code='+req.query.code+'&redirect_uri=https://crschatbot.herokuapp.com/'
 
 	}, function (error, response, body) {
-		console.log('++response++ : '+JSON.stringify(response)+' ++Body++ : '+ JSON.stringify(body);
+		console.log('++response++ : '+JSON.stringify(response)+' ++Body++ : '+ JSON.stringify(body));
 		if (!error && response.statusCode == 200) {
 			console.log('response : '+response+' Body : '+ body);
 			console.log("Success access_token++ : %s",response.access_token);
